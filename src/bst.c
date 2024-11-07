a73b0af6aaf152a25b6848537e048fc674f3a96b
Dylan Neve <dylanneve1@gmail.com>
2024-11-07 09:33:17 +0000

    Initial source dump

---
+++ src/bst.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bst.h"
+
+
+void tree_insert(Tree_Node** root, char data){
+    //TODO
+}
+
+Tree_Node* create_bst (char data[]){
+    // TODO
+}
+
+Tree_Node* tree_search(Tree_Node* root, char data){
+    //TODO
+    return NULL;
+}
+
+void tree_print_sorted(Tree_Node* root){
+    //TODO
+}
+
+void tree_delete(Tree_Node* root){
+    //TODO
+}
